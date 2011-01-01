d6dd32562d1319d882de65e63f32696599fbf5d4
google <google@firefant.com>
2011-01-01 16:55:18 -0500

    [no commit message]

---
+++ src/com/jstore/domain/Sesion.java
@@ -0,0 +1,116 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jstore.domain;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author phasnox
+ */
+@Entity
+@Table(name = "sesion")
+public class Sesion extends Generic implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_sesion")
+    private Integer idSesion;
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "minutos_maquina")
+    private Integer minutosMaquina;
+    @ManyToMany(mappedBy = "sesionList")
+    private List<Producto> productoList;
+    @OneToMany(mappedBy = "idSesion")
+    private List<Cita> citaList;
+
+    public Sesion() {
+    }
+
+    public Sesion(Integer idSesion) {
+        this.idSesion = idSesion;
+    }
+
+    public Integer getIdSesion() {
+        return idSesion;
+    }
+
+    public void setIdSesion(Integer idSesion) {
+        this.idSesion = idSesion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getMinutosMaquina() {
+        return minutosMaquina;
+    }
+
+    public void setMinutosMaquina(Integer minutosMaquina) {
+        this.minutosMaquina = minutosMaquina;
+    }
+
+    public List<Producto> getProductoList() {
+        return productoList;
+    }
+
+    public void setProductoList(List<Producto> productoList) {
+        this.productoList = productoList;
+    }
+
+    public List<Cita> getCitaList() {
+        return citaList;
+    }
+
+    public void setCitaList(List<Cita> citaList) {
+        this.citaList = citaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idSesion != null ? idSesion.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Sesion)) {
+            return false;
+        }
+        Sesion other = (Sesion) object;
+        if ((this.idSesion == null && other.idSesion != null) || (this.idSesion != null && !this.idSesion.equals(other.idSesion))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.jstore.domain.Sesion[idSesion=" + idSesion + "]";
+    }
+
+}
